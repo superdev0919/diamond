e3ebe526e4d8df990b7a8e447d3824babc7d520c
superdev0919 <superdev0919@gmail.com>
2023-04-20 05:15:21 +0300

    calculate volume and quality and weight of dimaond

--- script2.js
+++ script2.js
@@ -33,6 +33,11 @@ var panel;
 var points = vertices, lines = []
 for (var i = 0; i < vertices.length; i++) lines.push({ p1: i, p2: (i + 1) % vertices.length })
 
+//insert Unit line
+points.push({x: 125, y: 400}) , points.push({x: 375, y: 400})
+lines.push({p1: points.length - 2 , p2: points.length - 1})
+
+
 var cu_panel_mode = 1 //move circle mode, 2: move point mode, 3: scale circle mode , 4: connect point mode , 5: bend circle mode
 var selected_point = -1 // current selected point in move point mode
 var sp = -1, ep = -1 // start point and end point in connect point mode
@@ -51,7 +56,10 @@ var pc = {
     // line setting
     line_line_width: 2,
     line_color: 'red',
-    line_green_color: 'green'
+    line_green_color: 'green',
+
+    unit_length: 3.80,
+    density: 0.00352
 }
 
 var mc = {
@@ -98,10 +106,43 @@ function point(x, y) {
     panel.stroke();
 }
 
+function getParameters(){
+
+    var unit_line = pc.unit_length / distanceToPoint(points[7] , points[8])
+
+    var total_width = distanceToPoint(points[2], points[6])
+    var total_width_in_mm = total_width * unit_line
+    var table_width = distanceToPoint(points[lines[0].p1], points[lines[0].p2]) / total_width * 100
+    var total_depth = distToSegment(points[4], points[lines[0].p1] , points[lines[0].p2]) / total_width * 100
+    var crown_height = distToSegment(points[2], points[lines[0].p1] , points[lines[0].p2]) / total_width * 100
+    var pavilion_depth = distToSegment(points[4], points[3], points[5]) / total_width * 100
+    var girdle = (distToSegment(points[4], points[lines[0].p1] , points[lines[0].p2]) - distToSegment(points[2], points[lines[0].p1] , points[lines[0].p2]) - distToSegment(points[4], points[3], points[5])) / total_width * 100
+    var crown_angle_r = getAngle(points[1] , points[2] , points[3]) - 90
+    var crown_angle_l = getAngle(points[5] , points[6] , points[0]) - 90
+    var pavilion_angle_r = getAngle(points[2] , points[3] , points[4]) - 90
+    var pavilion_angle_l = getAngle(points[4] , points[5] , points[6]) - 90
+    return {
+        total_width: total_width,
+        total_width_in_mm: total_width_in_mm,
+        table_width: table_width,
+        total_depth: total_depth,
+        crown_height: crown_height,
+        pavilion_depth: pavilion_depth,
+        girdle: girdle,
+        crown_angle_r: crown_angle_r,
+        crown_angle_l: crown_angle_l,
+        pavilion_angle_r: pavilion_angle_r,
+        pavilion_angle_l: pavilion_angle_l,
+    }
+}
+
 function calculate() {
     var html = ""
 
+    var unit_line = distanceToPoint(points[7] , points[8]) / pc.unit_length
+
     var total_width = distanceToPoint(points[2], points[6])
+    var total_width_in_mm = total_width * unit_line
     var table_width = distanceToPoint(points[lines[0].p1], points[lines[0].p2]) / total_width * 100
     var total_depth = distToSegment(points[4], points[lines[0].p1] , points[lines[0].p2]) / total_width * 100
     var crown_height = distToSegment(points[2], points[lines[0].p1] , points[lines[0].p2]) / total_width * 100
@@ -113,8 +154,8 @@ function calculate() {
     var pavilion_angle_l = getAngle(points[4] , points[5] , points[6]) - 90
 
     html += `<tr>
-                <th scope="row">Total Width(px)</th>
-                <td>${total_width.toFixed(2)}</td>
+                <th scope="row">Total Width(mm)</th>
+                <td>${total_width_in_mm.toFixed(2)}</td>
             </tr>`
     html += `<tr>
                 <th scope="row">Table Width(% of Total Width)</th>
@@ -276,6 +317,18 @@ function rotate(cx, cy, x, y, angle) {
     return { x: nx, y: ny };
 }
 
+function getOverviewHtml(id){
+    var html = ''
+    for (var i = 0 ; i < id ; i ++) html += "<td></td>"
+    html += `<td>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                    <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                </svg>
+            </td>`
+    for (var i = id + 1 ; i < 7 ; i ++) html += '<td></td>'
+    return html
+}
+
 $(document).ready(() => {
 
     panel = document.getElementById("panel").getContext("2d")
@@ -387,6 +440,133 @@ $(document).ready(() => {
     })
     $("#reset_panel").on("click", () => {
         window.location.reload()
+    })
+    $("#overview").on("click", () => {
+        d = getParameters()
+
+        var crown_angle_html;
+        if ((d.crown_angle_r + d.crown_angle_l)/2 < 25.9) crown_angle_html = getOverviewHtml(0)
+        else if ((d.crown_angle_r + d.crown_angle_l)/2 < 27.9) crown_angle_html = getOverviewHtml(1)
+        else if ((d.crown_angle_r + d.crown_angle_l)/2 < 31.2) crown_angle_html = getOverviewHtml(2)
+        else if ((d.crown_angle_r + d.crown_angle_l)/2 < 36.7) crown_angle_html = getOverviewHtml(3)
+        else if ((d.crown_angle_r + d.crown_angle_l)/2 < 38.2) crown_angle_html = getOverviewHtml(4)
+        else if ((d.crown_angle_r + d.crown_angle_l)/2 < 40.0) crown_angle_html = getOverviewHtml(5)
+        else crown_angle_html = getOverviewHtml(6)
+
+        var pavilin_angle_html;
+        if ((d.pavilion_angle_r + d.pavilion_angle_l)/2 < 38.4) pavilin_angle_html = getOverviewHtml(0)
+        else if ((d.pavilion_angle_r + d.pavilion_angle_l)/2 < 39.5) pavilin_angle_html = getOverviewHtml(1)
+        else if ((d.pavilion_angle_r + d.pavilion_angle_l)/2 < 40.5) pavilin_angle_html = getOverviewHtml(2)
+        else if ((d.pavilion_angle_r + d.pavilion_angle_l)/2 < 41.8) pavilin_angle_html = getOverviewHtml(3)
+        else if ((d.pavilion_angle_r + d.pavilion_angle_l)/2 < 42.1) pavilin_angle_html = getOverviewHtml(4)
+        else if ((d.pavilion_angle_r + d.pavilion_angle_l)/2 < 43.1) pavilin_angle_html = getOverviewHtml(5)
+        else pavilin_angle_html = getOverviewHtml(6)
+
+        var table_width_html;
+        if (d.table_width < 47) table_width_html = getOverviewHtml(0)
+        else if (d.table_width < 49) table_width_html = getOverviewHtml(1)
+        else if (d.table_width < 51) table_width_html = getOverviewHtml(2)
+        else if (d.table_width < 62) table_width_html = getOverviewHtml(3)
+        else if (d.table_width < 66) table_width_html = getOverviewHtml(4)
+        else if (d.table_width < 70) table_width_html = getOverviewHtml(5)
+        else table_width_html = getOverviewHtml(6)
+
+        var crown_height_html;
+        if (d.crown_height < 8.5) crown_height_html = getOverviewHtml(0)
+        else if (d.crown_height < 10.5) crown_height_html = getOverviewHtml(1)
+        else if (d.crown_height < 11.5) crown_height_html = getOverviewHtml(2)
+        else if (d.crown_height < 17.0) crown_height_html = getOverviewHtml(3)
+        else if (d.crown_height < 18.0) crown_height_html = getOverviewHtml(4)
+        else if (d.crown_height < 19.5) crown_height_html = getOverviewHtml(5)
+        else crown_height_html = getOverviewHtml(6)
+
+        var pavilion_depth_html;
+        if (d.pavilion_depth < 39.5) pavilion_depth_html = getOverviewHtml(0)
+        else if (d.pavilion_depth < 41.0) pavilion_depth_html = getOverviewHtml(1)
+        else if (d.pavilion_depth < 42.5) pavilion_depth_html = getOverviewHtml(2)
+        else if (d.pavilion_depth < 44.5) pavilion_depth_html = getOverviewHtml(3)
+        else if (d.pavilion_depth < 45.0) pavilion_depth_html = getOverviewHtml(4)
+        else if (d.pavilion_depth < 46.5) pavilion_depth_html = getOverviewHtml(5)
+        else pavilion_depth_html = getOverviewHtml(6)
+
+        var girdle_html;
+        if (d.girdle < 0.5) girdle_html = getOverviewHtml(0)
+        else if (d.girdle < 1.5) girdle_html = getOverviewHtml(1)
+        else if (d.girdle < 2.0) girdle_html = getOverviewHtml(2)
+        else if (d.girdle < 4.5) girdle_html = getOverviewHtml(3)
+        else if (d.girdle < 5.5) girdle_html = getOverviewHtml(4)
+        else if (d.girdle < 7.5) girdle_html = getOverviewHtml(5)
+        else girdle_html = getOverviewHtml(6)
+
+        var total_depth_html;
+        if (d.total_depth < 52.9) total_depth_html = getOverviewHtml(0)
+        else if (d.total_depth < 55.4) total_depth_html = getOverviewHtml(1)
+        else if (d.total_depth < 58.4) total_depth_html = getOverviewHtml(2)
+        else if (d.total_depth < 63.5) total_depth_html = getOverviewHtml(3)
+        else if (d.total_depth < 64.4) total_depth_html = getOverviewHtml(4)
+        else if (d.total_depth < 66.9) total_depth_html = getOverviewHtml(5)
+        else total_depth_html = getOverviewHtml(6)
+
+        var sumalphabet_html;
+        if ((d.crown_angle_r + d.crown_angle_l)/2 + (d.pavilion_angle_r + d.pavilion_angle_l)/2 < 67.9) sumalphabet_html = getOverviewHtml(0)
+        else if ((d.crown_angle_r + d.crown_angle_l)/2 + (d.pavilion_angle_r + d.pavilion_angle_l)/2 < 69.9) sumalphabet_html = getOverviewHtml(1)
+        else if ((d.crown_angle_r + d.crown_angle_l)/2 + (d.pavilion_angle_r + d.pavilion_angle_l)/2 < 72.4) sumalphabet_html = getOverviewHtml(2)
+        else if ((d.crown_angle_r + d.crown_angle_l)/2 + (d.pavilion_angle_r + d.pavilion_angle_l)/2 < 77.7) sumalphabet_html = getOverviewHtml(3)
+        else if ((d.crown_angle_r + d.crown_angle_l)/2 + (d.pavilion_angle_r + d.pavilion_angle_l)/2 < 79.4) sumalphabet_html = getOverviewHtml(4)
+        else if ((d.crown_angle_r + d.crown_angle_l)/2 + (d.pavilion_angle_r + d.pavilion_angle_l)/2 < 80.4) sumalphabet_html = getOverviewHtml(5)
+        else sumalphabet_html = getOverviewHtml(6)
+
+        var html = `<tr>
+                        <th scope="row">Crown Angle (\u03B2)</th>
+                        ${crown_angle_html}
+                    </tr>
+                    <tr>
+                        <th scope="row">Pavilion Angle (\u2C6D)</th>
+                        ${pavilin_angle_html}
+                    </tr>
+                    <tr class="table-group-divider">
+                        <th scope="row">Table Width</th>
+                        ${table_width_html}
+                    </tr>
+                    <tr>
+                        <th scope="row">Crown Height</th>
+                        ${crown_height_html}
+                    </tr>
+                    <tr>
+                        <th scope="row">Pavilion Depth</th>
+                        ${pavilion_depth_html}
+                    </tr>
+                    <tr>
+                        <th scope="row">Girdle</th>
+                        ${girdle_html}
+                    </tr>
+                    <tr>
+                        <th scope="row">Total Depth</th>
+                        ${total_depth_html}
+                    </tr>
+                    <tr class="table-group-divider">
+                        <th scope="row">Sum \u2C6D and \u03B2</th>
+                        ${sumalphabet_html}
+                    </tr>`
+
+        $("#tbl_overview").empty(), $("#tbl_overview").append(html)
+
+        var top_r = d.total_width_in_mm * d.table_width / 100 / 2
+        var girdle_r = d.total_width_in_mm
+        var top_h = d.total_width_in_mm * d.crown_height / 100 / 2
+        var volume_top = Math.PI * top_h / 3 * (top_r * top_r + girdle_r * girdle_r + top_r * girdle_r)
+
+        var volume_girdle = Math.PI * girdle_r * girdle_r * d.girdle * d.total_width_in_mm / 100
+
+        var volume_bottom = Math.PI * girdle_r * girdle_r / 3
+
+        var diamond_volume = volume_top + volume_girdle + volume_bottom
+
+        var diamond_weight = diamond_volume * pc.density
+
+        $("#diamond_volume").empty() , $("#diamond_volume").append(diamond_volume.toFixed(2))
+        $("#diamond_weight").empty() , $("#diamond_weight").append(diamond_weight.toFixed(2))
+
     })
 
     $("#rotate_ccw").on('click', () => {
